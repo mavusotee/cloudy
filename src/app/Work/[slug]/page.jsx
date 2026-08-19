549b41917097ea6dc96db18f878bb83f7d17d2c0
Thokozani Mavuso <1mavusothokozani@gmail.com>
2026-08-19 17:26:14 +0200

    height

--- src/app/Work/[slug]/page.jsx
+++ src/app/Work/[slug]/page.jsx
@@ -691,7 +691,7 @@ export default function CloudhausWorkDetail() {
   return (
     <main
       className="
-        min-h-dvh
+        h-dvh
         bg-black
         text-zinc-300
         font-geist-mono
